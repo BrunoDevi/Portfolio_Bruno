e909b24e773e04ce48860f1eb69c7e945f16b4be
Bruno Baêta <brunoantonio25@outlook.com>
2024-02-27 21:00:35 -0300

    files changes

---
+++ index.js
@@ -0,0 +1,55 @@
+// ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
+const smallMenu = document.querySelector('.header__sm-menu')
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const headerHamMenuCloseBtn = document.querySelector(
+  '.header__main-ham-menu-close'
+)
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active')
+  } else {
+    smallMenu.classList.add('header__sm-menu--active')
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  } else {
+    headerHamMenuBtn.classList.add('d-none')
+    headerHamMenuCloseBtn.classList.remove('d-none')
+  }
+})
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active')
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  })
+}
+
+// ---
+const headerLogoConatiner = document.querySelector('.header__logo-container')
+
+headerLogoConatiner.addEventListener('click', () => {
+  location.href = 'index.html'
+})
+
+//BOTÃO 'TOPO' - start
+function Top() { // adiciona a funcão de voltar ao topo da pagina
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+var botaoTopo = document.querySelector('.botao-topo');
+
+window.addEventListener('scroll', function() {
+  // Verifica se a posição de rolagem é maior que a 1/4 da altura da janela
+  if (window.scrollY > window.innerHeight / 4) {
+      botaoTopo.style.opacity = 1;
+  } else {
+      botaoTopo.style.opacity = 0;
+  }
+});
+//BOTÃO 'TOPO' - end
